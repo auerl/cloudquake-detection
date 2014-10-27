e497f004fea6db80e9ae3ad87fbf5aac4cef4824
root <root@ip-172-31-40-5.ec2.internal>
2014-10-27 12:03:14 +0000

    cleaning up

--- src/main/scala/CloudQuakeSparkConsumer.scala
+++ src/main/scala/CloudQuakeSparkConsumer.scala
@@ -1,4 +1,22 @@
-/* SimpleApp.scala */
+/* 
+
+   CloudQuake Spark consumer
+
+   ======================================
+
+   * Ingests kinesis streams (with the stream name 
+     being the first cmd-line argument), pushed to
+     Kinesis by various producers.
+   * Applies our processing logic to the stream of
+     tweets. See Earle et al. (2011) for details.
+   * Requires environment variables AWS_ACCESS_KEY_ID 
+     and AWS_SECRET_KEY to be set.
+ 
+     Authors: Ludwig Auer, Shoma Arita, Lin Shen Husan
+  
+*/
+
+
 import java.nio.ByteBuffer
 import scala.util.Random
 import org.apache.spark.Logging
@@ -34,7 +52,6 @@ object CloudQuakeSparkConsumer {
      }
 
 
-     // =========================================
      /* Populate the appropriate variables from the given args */
      val Array(streamName, endpointUrl) = args
      println("Stream to connect: %s, Amazon backend: %s".format(streamName, endpointUrl))
@@ -45,16 +62,18 @@ object CloudQuakeSparkConsumer {
      val numShards = kinesisClient.describeStream(streamName).getStreamDescription().getShards()
       .size()
 
-     /* In this example, we're going to create 1 Kinesis Worker/Receiver/DStream for each shard. */
+     /* For now use 1 Kinesis Worker/Receiver/DStream for each shard. */
      val numStreams = numShards
 
      /* Setup the and SparkConfig and StreamingContext */
      /* Spark Streaming batch interval */
-     val batchInterval = Milliseconds(2000)
-     val sparkConfig = new SparkConf().setAppName("KinesisWordCount")
+     val batchInterval = Milliseconds(10000) // 10 seconds batchInterval
+     val sparkConfig = new SparkConf().setAppName("CloudQuakeSparkConsumer")
+                                      .set("spark.cleaner.ttl","7200") // clean every 2 hours 
      val ssc = new StreamingContext(sparkConfig, batchInterval)
      val sc = new SparkContext(sparkConfig)
 
+     /* Start up a new Spark SQL context */
      val sqlContext = new org.apache.spark.sql.SQLContext(sc)	
 
 
@@ -80,28 +99,27 @@ object CloudQuakeSparkConsumer {
      val hashTags = unionStreams.flatMap(byteArray => new String(byteArray).split(" ").filter(_.startsWith("#")))
 
 
-     /*
-
      /* list of words to search for */
+//     val keyWords = List("this")
+//     val containsthis1 = unionStreams.flatMap(byteArray => new String(byteArray).split(" ").filter(_=="this"))
+//     val containsthis2 = unionStreams.flatMap(byteArray => new String(byteArray).split(" ").filter(_.exists(keyWords contains _)))
 
-     */
-     val keyWords = List("this")
-     val containsthis1 = unionStreams.flatMap(byteArray => new String(byteArray).split(" ").filter(_=="this"))
-     val containsthis2 = unionStreams.flatMap(byteArray => new String(byteArray).split(" ").filter(_.exists(keyWords contains _)))
-     val countalltweets = unionStreams.flatMap(byteArray => new String(byteArray).split("\n")).countByWindow(Seconds(60),Seconds(2))
-//     containsthis1.countByWindow(Seconds(60),Seconds(2)).print()
-//     containsthis2.countByWindow(Seconds(60),Seconds(2)).print()
+//     containsthis1.countByWindow(Seconds(60),batchInterval).print()
+//     containsthis2.countByWindow(Seconds(60),batchInterval).print()
+
+     val countalltweets = unionStreams.flatMap(byteArray => new String(byteArray).split("\n"))
+                                        .countByWindow(Seconds(60),batchInterval)
      countalltweets.print()
 
 
      
      // Most popular hashtags in the last 60 seconds
-     val topCounts60 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60))
+     val topHashCounts60 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60))
      	              .map{case (topic, count) => (count, topic)}
               	      .transform(_.sortByKey(false))
 
      // Most popular hashtags in the last 10 seconds
-     val topCounts10 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(10))
+     val topHashCounts10 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(10))
      	              .map{case (topic, count) => (count, topic)}
               	      .transform(_.sortByKey(false))
 
@@ -110,76 +128,54 @@ object CloudQuakeSparkConsumer {
      	              .map{case (topic, count) => (count, topic)}
               	      .transform(_.sortByKey(false))
 
-
-// Most popular words in the last 60 seconds
-//     val topEbolaCounts60 = containsEbola.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60))
-//     	              .map{case (topic, count) => (count, topic)}
+      val windowed_dstream = lines.window(Seconds(60),batchInterval)
 
 
-      val windowed_dstream = lines.window(Seconds(60),Seconds(2))
+      // Print popular hashtags
+      topHashCounts60.foreachRDD(rdd => {
+      val topList = rdd.take(3)
+      println("\nTop 3 topics in last 60 seconds (%s total):".format(rdd.count()))
+      topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
+      })     
 
+      /*
+      topHashCounts10.foreachRDD(rdd => {
+      val topList = rdd.take(3)
+      println("\nTop 3 topics in last 10 seconds (%s total):".format(rdd.count()))
+      topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
+      })
+
+      topWordCounts60.foreachRDD(rdd => {
+      val topList = rdd.take(3)
+      println("\nTop 3 words topics in last 60 seconds (%s total):".format(rdd.count()))
+      topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
+      })
+      */
+
+      /* Now our processing logic starts */
       windowed_dstream.foreachRDD(rdd => {
            if (rdd.count > 0) {
                val json_rdd = sqlContext.jsonRDD(rdd)
-               json_rdd.registerAsTable("data_table")
-               json_rdd.printSchema()
+               json_rdd.registerAsTable("twitter_json")
+	       json_rdd.printSchema()
 
-               // rules
-	       // val query1_results = sqlContext.sql("SELECT * FROM data_table WHERE content LIKE '%this%' ")
- 	       // val query1_results = sqlContext.sql("SELECT * FROM data_table")
-	       val query1_results = sqlContext.sql("SELECT COUNT(*) FROM data_table").collect().head.getLong(0)
+               // Some sample requests on the data
+	       // val query1_results = sqlContext.sql("SELECT * FROM twitter_json WHERE content LIKE '%this%' ")
+ 	       // val query1_results = sqlContext.sql("SELECT * FROM twitter_json")
+	       val query1_results = sqlContext.sql("SELECT COUNT(*) FROM twitter_json").collect().head.getLong(0)
+
 	       // val contains_this = query1_results.collect.map(row => row.getString(5)) //get time
 	       // println("Stocks above market value: \n ======================= \n" + contains_this.mkString(",")) 
                println(s"Number of tweets per minute: $query1_results") 
 
               }
            })
-
-     /*
-
-      // Print popular hashtags
-      topCounts60.foreachRDD(rdd => {
-      val topList = rdd.take(10)
-      println("\nPopular topics in last 60 seconds (%s total):".format(rdd.count()))
-      topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
-      })
-
-
-      // Print tweets with ebola
-      topEbolaCounts60.foreachRDD(rdd => {
-      val topList = rdd.take(10)
-      println("\nPopular topics in last 60 seconds (%s total):".format(rdd.count()))
-      topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
-      })
+       
      
 
-      // Print popular hashtags
-      topWordCounts60.foreachRDD(rdd => {
-      val topList = rdd.take(10)
-      println("\nPopular topics in last 60 seconds (%s total):".format(rdd.count()))
-      topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
-      })
-
-
-      topCounts10.foreachRDD(rdd => {
-      val topList = rdd.take(10)
-      println("\nPopular topics in last 10 seconds (%s total):".format(rdd.count()))
-      topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
-      })
-
-
-      */
-
-
-     /* Now our processing logic starts, for now I just count popular hashtags 
-        another idea might be to count the most popular words. But will require
-        analysing the json format. How can I open such a format.
-     */
-     
-
-    /* Start the streaming context and await termination */
-    ssc.start()
-    ssc.awaitTermination()
+      /* Start the streaming context and await termination */
+      ssc.start()
+      ssc.awaitTermination()
 
 
 
